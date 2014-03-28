0b0d658623c382e26d7a8b32f76c7b55b3b6c8c6
Floyd Pink <imbleedingme@googlemail.com>
2014-03-28 15:38:21 -0400

    tweak build file for Linux; made go.sh executable

--- build/go.sh
+++ build/go.sh
@@ -4,7 +4,7 @@ buildfile="FlickrDownloadr.build"
 if [ "dummy" = "dummy$2" ]; then
     mono $nant -buildfile:$buildfile $1 -D:project.build.type=Debug
 else
-    mono $nant -buildfile:$buildfile $1 -D:project.build.type=$2    
+    mono $nant -buildfile:$buildfile $1 -D:project.build.type=$2
 fi
 
 date
