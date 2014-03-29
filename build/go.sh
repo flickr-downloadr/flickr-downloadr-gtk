9d9a12bcb7e8cac15754b87523957cdb7bff8573
Floyd Pink <imbleedingme@googlemail.com>
2014-03-28 20:33:55 -0400

    fix the issue of nant not being able to do pkg-config on mono

--- build/go.sh
+++ build/go.sh
@@ -1,6 +1,8 @@
 nant="nant/nant-0.92/bin/NAnt.exe"
 buildfile="FlickrDownloadr.build"
 
+export PKG_CONFIG_PATH=/opt/local/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig
+
 if [ "dummy" = "dummy$2" ]; then
     mono $nant -buildfile:$buildfile $1 -D:project.build.type=Debug
 else
