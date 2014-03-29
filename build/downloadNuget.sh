d1e679ba877836f9abc13f371374f58462d84e6c
Floyd Pink <imbleedingme@googlemail.com>
2014-03-29 01:18:16 -0400

    a minor typo in downloadNuget.sh [ci skip]

--- build/downloadNuget.sh
+++ build/downloadNuget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f nuget.exe
 wget http://nuget.org/nuget.exe
-chmod a+x .nuget.exe
+chmod a+x nuget.exe
 cd ../source
 mono ../build/nuget.exe restore "FloydPink.Flickr.Downloadr.sln" -verbosity detailed
