d86f93d881371b5dfbd87db9b5b55ccde6569d15
Floyd Pink <imbleedingme@googlemail.com>
2014-03-28 20:10:50 -0400

    removed go.bat and nuget.exe

--- build/go.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-IF dummy==dummy%2 (
-nant\nant-0.92\bin\NAnt.exe -buildfile:FlickrDownloadr.build %1 -D:project.build.type=Debug
-) ELSE (
-nant\nant-0.92\bin\NAnt.exe -buildfile:FlickrDownloadr.build %1 -D:project.build.type=%2
-)
-date /t && time /t
