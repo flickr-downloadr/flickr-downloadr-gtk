91374f5f9b32c286a4e1cf8d7705c22c23ea703e
Floyd Pink <imbleedingme@googlemail.com>
2014-04-14 12:43:13 -0400

    we're building an exe that is somehow failing

--- build/create-release-win.ps1
+++ build/create-release-win.ps1
@@ -1,10 +1,10 @@
-$solutionDir = "..\source"                      # this is where my code lives
-$buildOutputDir = "..\source\bin\Release" # this is where the build output lives
-$releasesDir = ".\Release"          # publish to a folder outside the repository
+$solutionDir = "..\source"                 # this is where my code lives
+$buildOutputDir = "..\source\bin\Release"  # this is where the build output lives
+$releasesDir = "..\dist\win"                 # publish to a folder outside the repository
 
 $script =  Get-ChildItem "$solutionDir\packages\\" `
                 -Filter "Create-Release.ps1" `
                 -Recurse | Select-Object -first 1
 
-. $script.FullName -SolutionDir $solutionDir -BuildDir $buildOutputDir ` 
+. $script.FullName -SolutionDir $solutionDir -BuildDir $buildOutputDir `
                    -ReleasesDir $releasesDir
