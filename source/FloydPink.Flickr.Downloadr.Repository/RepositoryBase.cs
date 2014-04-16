d867df31e250eccc9cdc86a0e7f1b48a2789a7e2
Floyd Pink <imbleedingme@googlemail.com>
2014-04-16 15:13:44 -0400

    now writing the repos to appdata; removed buildpackage from csproj to stop building nupkg; bitrock installer is building an exe for windows

--- source/FloydPink.Flickr.Downloadr.Repository/RepositoryBase.cs
+++ source/FloydPink.Flickr.Downloadr.Repository/RepositoryBase.cs
@@ -1,33 +1,44 @@
+using System;
 using System.IO;
 using FloydPink.Flickr.Downloadr.Repository.Helpers;
 
 namespace FloydPink.Flickr.Downloadr.Repository
 {
-    public abstract class RepositoryBase
-    {
-        protected readonly string CryptKey = "SomeEncryPtionKey123";
-        internal abstract string RepoFileName { get; }
+	public abstract class RepositoryBase
+	{
+		protected readonly string CryptKey = "SomeEncryPtionKey123";
 
-        public void Delete()
-        {
-            if (File.Exists(RepoFileName))
-            {
-                File.Delete(RepoFileName);
-            }
-        }
+		internal abstract string RepoFileName { get; }
 
-        protected string Read()
-        {
-            if (File.Exists(RepoFileName))
-            {
-                return Crypt.Decrypt(File.ReadAllText(RepoFileName), CryptKey);
-            }
-            return string.Empty;
-        }
+		string _appDataFolder = Path.Combine(Environment.GetFolderPath (Environment.SpecialFolder.ApplicationData), "flickr-downloadr");
 
-        protected void Write(string fileContent)
-        {
-            File.WriteAllText(RepoFileName, Crypt.Encrypt(fileContent, CryptKey));
-        }
-    }
+		private string AbsoluteFilePath {
+			get {
+				return Path.Combine (_appDataFolder, RepoFileName);
+			}
+		}
+
+		public void Delete ()
+		{
+			if (File.Exists (AbsoluteFilePath)) {
+				File.Delete (AbsoluteFilePath);
+			}
+		}
+
+		protected string Read ()
+		{
+			if (File.Exists (AbsoluteFilePath)) {
+				return Crypt.Decrypt (File.ReadAllText (AbsoluteFilePath), CryptKey);
+			}
+			return string.Empty;
+		}
+
+		protected void Write (string fileContent)
+		{
+			if (!Directory.Exists (_appDataFolder)) {
+				Directory.CreateDirectory (_appDataFolder);
+			}
+			File.WriteAllText (AbsoluteFilePath, Crypt.Encrypt (fileContent, CryptKey));
+		}
+	}
 }
