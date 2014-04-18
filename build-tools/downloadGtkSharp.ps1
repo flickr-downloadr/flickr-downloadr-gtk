faa55a421640bf4f31f1deefec00d34fe0f5231c
Floyd Pink <imbleedingme@googlemail.com>
2014-04-18 12:33:34 -0400

    appveyor will not work with relative paths

--- build-tools/downloadGtkSharp.ps1
+++ build-tools/downloadGtkSharp.ps1
@@ -1,2 +1,2 @@
-(new-object System.Net.WebClient).DownloadFile('http://flickrdownloadr.com/installer/win/dependencies/gtk-sharp-2.12.25.msi','./gtk-sharp-2.12.25.msi')
-msiexec /i gtk-sharp-2.12.25.msi /quiet /qn /norestart /log install.log
+(new-object System.Net.WebClient).DownloadFile('http://flickrdownloadr.com/installer/win/dependencies/gtk-sharp-2.12.25.msi','C:\projects\flickr-downloadr-gtk\gtk-sharp-2.12.25.msi')
+msiexec /i "C:\projects\flickr-downloadr-gtk\gtk-sharp-2.12.25.msi" /quiet /qn /norestart /log install.log
