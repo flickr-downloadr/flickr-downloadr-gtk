4eb9489f92951e6e83007f81010751f3b083ae8d
Floyd Pink <imbleedingme@googlemail.com>
2014-04-18 15:28:07 -0400

    added try catch and fixed the error in referencing variables

--- build-tools/install.ps1
+++ build-tools/install.ps1
@@ -1,17 +1,22 @@
-$buildToolsDir = "C:\projects\flickr-downloadr-gtk\build-tools\"
+try {
+	$buildToolsDir = "C:\projects\flickr-downloadr-gtk\build-tools\"
 
-$gtkInstaller = "gtk-sharp-2.12.25.msi"
-$gtkInstallerMsi = "http://flickrdownloadr.com/installer/win/dependencies/$(gtkInstaller)"
-$gtkInstallerMsiLocal = "$(buildToolsDir)$(gtkInstaller)"
-$gtkInstallerInstallLog = "$(buildToolsDir)gtkInstallerLog.log"
+	$gtkInstaller = "gtk-sharp-2.12.25.msi"
+	$gtkInstallerMsi = "http://flickrdownloadr.com/installer/win/dependencies/$($gtkInstaller)"
+	$gtkInstallerMsiLocal = "$($buildToolsDir)$($gtkInstaller)"
+	$gtkInstallerInstallLog = "$($buildToolsDir)gtkInstallerLog.log"
 
-$installBuilder = "installbuilder-enterprise-8.6.0-windows-installer.exe"
-$installBuilderExe = "http://installbuilder.bitrock.com/$(installBuilder)"
-$installBuilderExeLocal = "$(buildToolsDir)$(installBuilder)"
-$installBuilderInstallLog = "$(buildToolsDir)installbuilder.log"
+	$installBuilder = "installbuilder-enterprise-8.6.0-windows-installer.exe"
+	$installBuilderExe = "http://installbuilder.bitrock.com/$($installBuilder)"
+	$installBuilderExeLocal = "$($buildToolsDir)$($installBuilder)"
+	$installBuilderInstallLog = "$($buildToolsDir)installbuilder.log"
 
-(new-object System.Net.WebClient).DownloadFile($gtkInstallerMsi,$gtkInstallerMsiLocal)
-msiexec /i $gtkInstallerMsiLocal /quiet /qn /norestart /log $gtkInstallerInstallLog
+	(new-object System.Net.WebClient).DownloadFile($gtkInstallerMsi,$gtkInstallerMsiLocal)
+	msiexec /i $gtkInstallerMsiLocal /quiet /qn /norestart /log $gtkInstallerInstallLog
 
-(new-object System.Net.WebClient).DownloadFile($installBuilderExe,$installBuilderExeLocal)
-& $installBuilderExeLocal --mode unattended --unattendedmodeui none --debuglevel 4 --debugtrace $installBuilderInstallLog
+	(new-object System.Net.WebClient).DownloadFile($installBuilderExe,$installBuilderExeLocal)
+	& $installBuilderExeLocal --mode unattended --unattendedmodeui none --debuglevel 4 --debugtrace $installBuilderInstallLog
+}
+catch {
+	throw
+}
