2a7cafff45d4dfc8e92e286ac9d1c6a6cf43f15f
Floyd Pink <imbleedingme@googlemail.com>
2014-04-18 12:37:02 -0400

    hopefully this will work

--- build-tools/downloadGtkSharp.ps1
+++
@@ -1,2 +0,0 @@
-(new-object System.Net.WebClient).DownloadFile('http://flickrdownloadr.com/installer/win/dependencies/gtk-sharp-2.12.25.msi','C:\projects\flickr-downloadr-gtk\gtk-sharp-2.12.25.msi')
-msiexec /i "C:\projects\flickr-downloadr-gtk\gtk-sharp-2.12.25.msi" /quiet /qn /norestart /log install.log
