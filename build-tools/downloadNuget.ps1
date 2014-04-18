5aefd8076af6f2ce67920c02b1c6bb6a2b8535e3
Floyd Pink <imbleedingme@googlemail.com>
2014-04-17 23:14:28 -0400

    remove the restore package feature from the projects (and restore it in downloadNuget.ps1)

--- build-tools/downloadNuget.ps1
+++ build-tools/downloadNuget.ps1
@@ -1,2 +1,4 @@
 Remove-Item ../source/.nuget/NuGet.exe
 (new-object System.Net.WebClient).DownloadFile('http://nuget.org/nuget.exe','../source/.nuget/NuGet.exe')
+cd ../source
+../build/nuget.exe restore "FloydPink.Flickr.Downloadr.sln" -verbosity detailed
