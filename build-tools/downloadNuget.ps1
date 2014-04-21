ab4576fa212aed413c084c657e0b9a7d543a38e3
Floyd Pink <imbleedingme@googlemail.com>
2014-04-21 16:39:44 -0400

    added a check to remove the unnecessary error in powershell

--- build-tools/downloadNuget.ps1
+++ build-tools/downloadNuget.ps1
@@ -1,4 +1,6 @@
-Remove-Item ../source/.nuget/NuGet.exe
+if (Test-Path ../source/.nuget/NuGet.exe) {
+  Remove-Item ../source/.nuget/NuGet.exe
+}
 (new-object System.Net.WebClient).DownloadFile('http://nuget.org/nuget.exe','../source/.nuget/NuGet.exe')
 cd ../source
 ./.nuget/NuGet.exe restore "FloydPink.Flickr.Downloadr.sln" -verbosity detailed
