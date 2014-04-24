1fd701d313ffbd9c106f94ed588cfb0a447b6df4
Floyd Pink <imbleedingme@googlemail.com>
2014-04-24 17:15:05 -0400

    'appveyor now has the install builder license

--- build-tools/create-release-win.ps1
+++ build-tools/create-release-win.ps1
@@ -1,4 +1,5 @@
 $installBuilderCli = "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8.6.0\bin\builder-cli.exe"
 $pathToBuildFile = "flickrdownloadr.xml"
+$pathToLicenseFile = "flickrdownloadrlicense.xml"
 
-& $installBuilderCli build $pathToBuildFile
+& $installBuilderCli build $pathToBuildFile --license $pathToLicenseFile
