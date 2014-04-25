a1190495091a1fc51e47ad23e35d7d6b3471d386
Floyd Pink <imbleedingme@googlemail.com>
2014-04-25 12:46:35 -0400

    date time is what was causing the scripts to nt fail

--- build-tools/go.bat
+++ build-tools/go.bat
@@ -4,7 +4,8 @@ IF dummy==dummy%2 (
 ) ELSE (
   nant\nant-0.92\bin\NAnt.exe -buildfile:windows.build %1 -D:project.build.type=%2
 )
-echo 'errorlevel is ' %errorlevel%
-date /t && time /t
 
+echo 'errorlevel is ' %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+date /t && time /t
