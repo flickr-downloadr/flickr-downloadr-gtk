163854c2a14d5861906ec0c35b908d9a23b4f903
Floyd Pink <imbleedingme@googlemail.com>
2014-04-25 15:32:44 -0400

    added a mac release as well

--- build-tools/create-release.ps1
+++ build-tools/create-release.ps1
@@ -2,4 +2,10 @@ $installBuilderCli = "C:\Program Files (x86)\BitRock InstallBuilder Enterprise 8
 $pathToBuildFile = "flickrdownloadr.xml"
 $pathToLicenseFile = "flickrdownloadrlicense.xml"
 
+$platform = "windows"
+If($args.Length -eq 1)
+ {
+   $platform = $args[0]
+ }
+
 & $installBuilderCli build $pathToBuildFile --license $pathToLicenseFile
--- build-tools/go.bat
+++ build-tools/go.bat
@@ -5,7 +5,6 @@ IF dummy==dummy%2 (
   nant\nant-0.92\bin\NAnt.exe -buildfile:windows.build %1 -D:project.build.type=%2
 )
 
-echo 'errorlevel is ' %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 date /t && time /t
