f32aacbf9d2ab63356bc1dc56ecf01092cfbb925
Floyd Pink <imbleedingme@googlemail.com>
2014-04-25 12:14:16 -0400

    just print the errorlevel within go.bat before exiting

--- build-tools/go.bat
+++ build-tools/go.bat
@@ -4,5 +4,7 @@ IF dummy==dummy%2 (
 ) ELSE (
   nant\nant-0.92\bin\NAnt.exe -buildfile:windows.build %1 -D:project.build.type=%2
 )
+echo 'errorlevel is ' %errorlevel%
 date /t && time /t
+
 if %errorlevel% neq 0 exit /b %errorlevel%
