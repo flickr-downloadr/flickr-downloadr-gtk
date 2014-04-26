d73f2108cb010ef764a0849343daafa6c91fe30c
Floyd Pink <imbleedingme@googlemail.com>
2014-04-26 19:53:40 -0400

    now building form both win and mac (on local and CI(s))

--- build-tools/createRelease.sh
+++ build-tools/createRelease.sh
@@ -1,16 +1,14 @@
-INSTALLBUILDERCLI = "'/Applications/Bitrock InstallBuilder Enterprise 8.6.0/bin/Builder.app/Contents/MacOS/installbuilder.sh'"
-PATHTOBUILDFILE = "flickrdownloadr.xml"
-PATHTOLICENSEFILE = "flickrdownloadrlicense.xml"
+INSTALLBUILDERCLI="/Applications/Bitrock InstallBuilder Enterprise 8.6.0/bin/Builder.app/Contents/MacOS/installbuilder.sh"
+PATHTOBUILDFILE="flickrdownloadr.xml"
+PATHTOLICENSEFILE="flickrdownloadrlicense.xml"
 
-PLATFORM = "mac"
+PLATFORM="mac"
 if [ $# = 1 ]; then
-  PLATFORM = $1
+  PLATFORM=$1
 fi
-
-EXECCOMMAND = $INSTALLBUILDERCLI build $PATHTOBUILDFILE $PLATFORM
 
 if [ -f $PATHTOLICENSEFILE ]; then
-  EXECCOMMAND = $EXECCOMMAND $PLATFORM --license $PATHTOLICENSEFILE
+  eval "'$INSTALLBUILDERCLI' build $PATHTOBUILDFILE $PLATFORM --license $PATHTOLICENSEFILE"
+else
+  eval "'$INSTALLBUILDERCLI' build $PATHTOBUILDFILE $PLATFORM"
 fi
-
-$EXECCOMMAND
