75b3dcd462157a7b9542e95fe776fe05f15482fe
Floyd Pink <imbleedingme@googlemail.com>
2014-06-08 20:11:41 -0400

    change the mozroots import command slightly [deploy]

--- build-tools/apt-get-install.sh
+++ build-tools/apt-get-install.sh
@@ -3,4 +3,4 @@
 echo "Installing dependencies for flickr downloadr..."
 echo "Packages to be installed are: ${1}"
 sudo apt-get -y install ${1}
-mozroots --import --ask-remove
+mozroots --import --sync
