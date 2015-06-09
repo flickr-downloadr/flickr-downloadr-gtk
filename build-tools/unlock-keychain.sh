76e7cb258f2b5614cdc5531bb6feb458bcc28d81
Floyd Pink <imbleedingme@googlemail.com>
2015-06-09 13:07:30 -0400

    create a keychain on the fly and import the certificate into it

--- build-tools/unlock-keychain.sh
+++ build-tools/unlock-keychain.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-# Thanks to http://docs.travis-ci.com/user/common-build-problems/#Mac%3A-Code-Signing-Errors
+# Thanks to http://stackoverflow.com/a/27778139/218882
 
-KEY_CHAIN="./flickr-downloadr.keychain"
+KEY_CHAIN="flickr-downloadr.keychain"
+security create-keychain -p travis ${KEY_CHAIN}
 # Make the keychain the default so identities are found
 security default-keychain -s ${KEY_CHAIN}
 # Unlock the keychain
-security unlock-keychain -p ${TRAVIS_COMMIT} ${KEY_CHAIN}
+security unlock-keychain -p travis ${KEY_CHAIN}
 # Set keychain locking timeout to 3600 seconds
 security set-keychain-settings -t 3600 -u ${KEY_CHAIN}
+
+# Add certificates to keychain and allow codesign to access them
+security import "./X47E7X2X6J-code-signing-certificate.p12" -k ${KEY_CHAIN} -P ${TRAVIS_COMMIT} -T /usr/bin/codesign
