9d607414a0294f8c74a5be3fd816457cfd60daf5
Floyd Pink <imbleedingme@googlemail.com>
2016-12-04 16:32:38 -0500

    upgrades NAnt to the latest v0.93 nightly release

--- build-tools/go.bat
+++ build-tools/go.bat
@@ -1,8 +1,8 @@
 @echo off
 IF dummy==dummy%2 (
-  nant\nant-0.92\bin\NAnt.exe -buildfile:windows.build %1 -D:project.build.type=Debug
+  nant\nant-0.93-nightly-2015-02-12\bin\NAnt.exe -buildfile:windows.build %1 -D:project.build.type=Debug
 ) ELSE (
-  nant\nant-0.92\bin\NAnt.exe -buildfile:windows.build %1 -D:project.build.type=%2
+  nant\nant-0.93-nightly-2015-02-12\bin\NAnt.exe -buildfile:windows.build %1 -D:project.build.type=%2
 )
 
 if %errorlevel% neq 0 exit /b %errorlevel%
