cd83879ec3b76106205f73a0dd7a2361077393d2
Floyd Pink <imbleedingme@googlemail.com>
2016-12-05 17:31:50 -0500

    adds a local NuGet.Config so that wercker would not run into access issues for the global NuGet.Config

--- build-tools/downloadNuget.sh
+++ build-tools/downloadNuget.sh
@@ -4,4 +4,4 @@ wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 chmod a+x nuget.exe
 mv nuget.exe ../source/.nuget/NuGet.exe
 cd ../source
-mono ./.nuget/NuGet.exe restore "FloydPink.Flickr.Downloadr.sln" -verbosity detailed
+mono ./.nuget/NuGet.exe restore "FloydPink.Flickr.Downloadr.sln" -verbosity detailed -configFile NuGet.Config
