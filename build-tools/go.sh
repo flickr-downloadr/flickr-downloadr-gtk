880312926d7c5c8400a6137c605d8d18993c794c
Hari Menon <imbleedingme@googlemail.com>
2016-12-05 08:55:34 -0500

    use the custom built NAnt [deploy]

--- build-tools/go.sh
+++ build-tools/go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-nant="nant/nant-0.93-nightly-2015-02-12/bin/NAnt.exe"
+nant="nant/nant-0.93-custom-2016-12-05/bin/NAnt.exe"
 buildfile="posix.build"
 
 export PKG_CONFIG_PATH=/opt/local/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig
