433547ee48a99924c7f8314cfb41b08c0f7a9fb9
Floyd Pink <imbleedingme@googlemail.com>
2016-12-05 19:31:02 -0500

    attempts to run nuget download with sudo and update usr/localbin creation to not use sudo

--- build-tools/downloadNuget.ps1
+++ build-tools/downloadNuget.ps1
@@ -3,4 +3,4 @@ if (Test-Path ../source/.nuget/NuGet.exe) {
 }
 (new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe','../source/.nuget/NuGet.exe')
 cd ../source
-./.nuget/NuGet.exe restore "FloydPink.Flickr.Downloadr.sln" -verbosity detailed -configFile ../build-tools/NuGet.Config
+./.nuget/NuGet.exe restore -configFile ../build-tools/NuGet.Config -verbosity detailed "FloydPink.Flickr.Downloadr.sln"
--- build-tools/downloadNuget.sh
+++ build-tools/downloadNuget.sh
@@ -4,4 +4,4 @@ wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 chmod a+x nuget.exe
 mv nuget.exe ../source/.nuget/NuGet.exe
 cd ../source
-mono ./.nuget/NuGet.exe restore "FloydPink.Flickr.Downloadr.sln" -verbosity detailed -configFile ../build-tools/NuGet.Config
+sudo mono ./.nuget/NuGet.exe restore -configFile ../build-tools/NuGet.Config  -verbosity detailed "FloydPink.Flickr.Downloadr.sln"
