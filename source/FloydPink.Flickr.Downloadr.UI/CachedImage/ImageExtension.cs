c753087e063315ebc992dc13c5c3b0cc53761e94
robermann <robermann@gmail.com>
2017-07-30 21:05:32 +0200

    Fixed issue #53 - corrupted images in cache
    Now we try to recover in case of possible corrupted images, by deleting them and retrying a new read.

--- source/FloydPink.Flickr.Downloadr.UI/CachedImage/ImageExtension.cs
+++ source/FloydPink.Flickr.Downloadr.UI/CachedImage/ImageExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Gdk;
 using Image = Gtk.Image;
 
@@ -8,9 +9,48 @@ namespace FloydPink.Flickr.Downloadr.UI.CachedImage
   {
     public static void SetCachedImage(this Image self, string imageUrl)
     {
-      using (var file = new FileStream(FileCache.FromUrl(imageUrl), FileMode.Open))
+
+      int maxRetries = 2;
+      bool loaded = false;
+
+      for (int count = 1; !loaded; count++)
       {
-        self.Pixbuf = new Pixbuf(file);
+        using (var file = new FileStream(FileCache.FromUrl(imageUrl), FileMode.Open))
+        {
+          try
+          {
+            self.Pixbuf = new Pixbuf(file);
+            loaded = true;
+          }
+          catch (Exception e)
+          {
+
+            //possible corruption in a cached file, for example an partial aborted file.
+
+            if (count == maxRetries)
+            {
+              throw e;
+
+            }
+            else
+            {
+              file.Close();
+              HandeException(imageUrl, e);
+            }
+          }
+        }
+
+
+      }
+    }
+
+    private static void HandeException(string imageUrl, Exception e)
+    {
+      string corruptedFilename = FileCache.GetLocalFilename(new Uri(imageUrl));
+
+      if (File.Exists(corruptedFilename))
+      {
+        File.Delete(corruptedFilename);
       }
     }
   }
