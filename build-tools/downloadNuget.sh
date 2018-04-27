f9332141dfc6fe2eb3646515c304f7b5816036a7
Floyd Pink <imbleedingme@googlemail.com>
2018-04-27 02:23:58 -0400

    reverted conditional sudo [deploy]

--- build-tools/downloadNuget.sh
+++ build-tools/downloadNuget.sh
@@ -4,8 +4,4 @@ wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 chmod a+x nuget.exe
 mv nuget.exe ../source/.nuget/NuGet.exe
 cd ../source
-SUDO_CMD="sudo"
-if [[ $WERCKER = true ]]; then
-  SUDO_CMD=""
-fi
-$SUDO_CMD mono ./.nuget/NuGet.exe restore -configFile ../build-tools/NuGet.Config  -verbosity detailed "FloydPink.Flickr.Downloadr.sln"
+sudo mono ./.nuget/NuGet.exe restore -configFile ../build-tools/NuGet.Config  -verbosity detailed "FloydPink.Flickr.Downloadr.sln"
