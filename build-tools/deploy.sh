d792a0ee406a57cc6aa9d6fd606ab2b2afd0e50d
Floyd Pink <imbleedingme@googlemail.com>
2018-04-30 12:02:25 -0400

    change GH_TOKEN [deploy]

--- build-tools/deploy.sh
+++ build-tools/deploy.sh
@@ -18,7 +18,7 @@ then
   CIENGINE="wercker"
   wget "http://stedolan.github.io/jq/download/linux64/jq"
   chmod +x jq
-  echo "About to run: curl https://api.github.com/repos/flickr-downloadr/flickr-downloadr-gtk/commits/${WERCKER_GIT_COMMIT} | jq -r '.commit.message'"
+  echo "About to run: curl https://api.github.com/repos/flickr-downloadr/flickr-downloadr-gtk/commits/${WERCKER_GIT_COMMIT} | ./jq -r '.commit.message'"
   APPVEYOR_REPO_COMMIT_MESSAGE=$(curl -u ${GH_TOKEN}:x-oauth-basic https://api.github.com/repos/flickr-downloadr/flickr-downloadr-gtk/commits/$WERCKER_GIT_COMMIT | ./jq -r '.commit.message')
 fi
 
