37f6837399884dd9537b18fa3a810896f0dffb6e
Sindhu Vijayan <sindhu@flickrdownloadr.com>
2019-06-01 22:27:09 -0400

    update install builder to 19.5.0 [deploy]

--- build-tools/createRelease.sh
+++ build-tools/createRelease.sh
@@ -1,4 +1,4 @@
-INSTALL_BUILDER_VERSION="18.10.0"
+INSTALL_BUILDER_VERSION="19.5.0"
 
 if [ "$(uname)" == "Darwin" ]; then
   INSTALLBUILDERCLI="/Applications/Bitrock InstallBuilder Enterprise ${INSTALL_BUILDER_VERSION}/bin/Builder.app/Contents/MacOS/installbuilder.sh"
